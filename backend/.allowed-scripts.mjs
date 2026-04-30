c9f85fadc3b321303fcf3bba9f162bff501bd6e0
drummondjm <mark.drummond@justice.gov.uk>
2026-04-30 15:00:54 +0100

    implementing hmpps-npm-script-allowlist

---
+++ backend/.allowed-scripts.mjs
@@ -0,0 +1,23 @@
+import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
+
+export default configureAllowedScripts({
+   allowlist: {
+      // Medusa telemetry - both versions
+      'node_modules/@medusajs/telemetry@2.14.0': 'FORBID',
+      'node_modules/@medusajs/medusa/node_modules/@medusajs/telemetry@2.14.1': 'FORBID',
+
+      // Build tools
+      'node_modules/@swc/core@1.15.32': ['postinstall'],
+
+      // Native modules
+      'node_modules/msgpackr-extract@3.0.3': ['install'],
+
+      // Protocol buffers
+      'node_modules/protobufjs@7.5.6': ['postinstall'],
+
+      // ESBuild is written in GoLang - this is needed to download prebuilt binaries for the specific platform
+      'node_modules/esbuild@0.21.5': 'ALLOW',
+      // Needed by jest for running tests in watch mode
+      'node_modules/fsevents@2.3.3': 'ALLOW',
+   },
+})
