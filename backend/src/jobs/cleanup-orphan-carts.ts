0a9b5375d07ae52bcd1575111e6bea2991c0ca97
neeta.shinde <neeta.shinde@justice.gov.uk>
2026-09-24 11:01:27 +0100

    updated schduler run time for testing

--- backend/src/jobs/cleanup-orphan-carts.ts
+++ backend/src/jobs/cleanup-orphan-carts.ts
@@ -40,5 +40,5 @@ export default async function cleanupOrphanCarts(container: MedusaContainer) {
 
 export const config = {
   name: 'cleanup-orphan-carts',
-  schedule: '0 11 * * *',
+  schedule: '15 11 * * *',
 }
